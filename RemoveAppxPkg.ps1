7a542c95d97311c014a14f8b987cf36a4084adca
dotchetter <dotchetter@protonmail.ch>
2018-11-15 17:33:16 +0100

    Added functionality for specific demand to unpin OneDrive from explorer pane. Script is designed to run on a newly enrolled machine into MDM.
    ** TODOS **
    
    Find working way to suppress actions and resume if userprofile is defaultuser0

--- RemoveAppxPkg.ps1
+++ RemoveAppxPkg.ps1
@@ -1,32 +1,88 @@
 
-# Uninstalls Apps in array and removes provisioned packages to prevent automatic re-install 
 
-[array]$all_apps = Get-AppxPackage | Select Name
-[array]$rem_arr = @(
+[int]$counter = 0
+[array]$ALL_APPS = Get-AppxPackage | Select Name
+[array]$REGVALUES = @(
 
-    "*communicationsapps*",
-    "*messaging*","*windowsfeedback*",
-    "*skype*","*print3d*",
-    "*getstarted*","*onedrive*",
-    "*windowsmaps*","*xboxidentityprovider*",
-    "*mspaint*","*xboxapp*",
-    "*bingfinance*","*bingnews*",
-    "*bingweather*","*gethelp*",
-    "*solitairecollection*","*stickynotes*",
-    "*photos*","*oneconnect*",
-    "*xboxspeechtotextoverlay*","*xbox.tcui*"
-    "*wallet*","*zunemusic*",
-    "*storepurchaseapp*","*microsoft3dviewer*",
-    "*onenote*","*bingsports*",
-    "*officehub*","*zunevideo*"
+    'HKEY_CLASSES_ROOT\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}',
+    'HKEY_CLASSES_ROOT\WOW6432Node\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}'
+);
+[array]$REM_ARR = @(
+
+    '*communicationsapps*',
+    '*messaging*','*windowsfeedback*',
+    '*skype*','*print3d*',
+    '*getstarted*','*onedrive*',
+    '*windowsmaps*','*xboxidentityprovider*',
+    '*mspaint*','*xboxapp*',
+    '*bingfinance*','*bingnews*',
+    '*bingweather*','*gethelp*',
+    '*solitairecollection*','*stickynotes*',
+    '*photos*','*oneconnect*',
+    '*xboxspeechtotextoverlay*','*xbox.tcui*'
+    '*wallet*','*zunemusic*',
+    '*storepurchaseapp*','*microsoft3dviewer*',
+    '*onenote*','*bingsports*',
+    '*officehub*','*zunevideo*'
 );
 
-function rmvapp {param($app)
-    
-    Get-AppxPackage -allusers -name $app | Remove-AppxPackage
-    Get-AppxProvisionedPackage -Online | where {$_.packagename -like '$app'} | 
-    Remove-AppxProvisionedPackage -Online
+
+# find out if process process is running, return boolean
+function alive {param($process)
+
+    $status = get-process | where {$_.name -like '$process'}
+    if ($status) {
+        $its_alive = $true
+    }
+    else {
+        $its_alive = $false
+    }
+    return $its_alive
 }
 
-foreach ($i in $all_apps) {
-    foreach ($j in $rem_arr) {if ($i -like $j) {rmvapp($j)}}}
+
+# uninstalls Apps in array and removes provisioned packages to prevent automatic re-install 
+function remove_app {param($app)
+    
+    get-appxpackage -allusers -name $app | Remove-AppxPackage
+    get-appxprovisionedpackage -Online | where {$_.packagename -like '$app'} | 
+    remove-appxprovisionedpackage -Online
+}
+
+
+# modify registry
+function regmod {param($values)
+
+    foreach ($value in $values) {
+        reg delete $value /v System.IsPinnedToNameSpaceTree /f
+        gpupdate /force
+        start-sleep(3)
+        reg add $value /v System.IsPinnedToNameSpaceTree /t REG_DWORD /d 0 /f
+    }
+}
+
+
+# main
+function main {
+
+    if ($env:username -ne 'defaultuser0') {
+        $is_alive = alive('onedrive')
+        if ($is_alive -eq $true) {
+            get-process | where {$_.name -like 'onedrive'} | stop-process
+        }
+        regmod($REGVALUES)
+        foreach ($i in $ALL_APPS) {
+            foreach ($j in $REM_ARR) {if ($i -like $j) {remove_app($j)}
+            }
+        }
+    }
+    else {
+        $counter += 1
+        start-sleep(60)
+        if ($counter -lt 20) {main}
+        else {exit}
+    }
+}
+
+main
+if ($Error) {$Error | out-file $env:systemdrive\temp\awscript\error.log}
