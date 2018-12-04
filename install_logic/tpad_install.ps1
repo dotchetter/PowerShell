9c9b24694e0f2749c44baa7e09831ae8ae9e173d
dotchetter <dotchetter@protonmail.ch>
2018-12-04 11:32:42 +0100

    Error handling function for easy code reuse

--- install_logic/tpad_install.ps1
+++ install_logic/tpad_install.ps1
@@ -9,18 +9,23 @@
 
 function main {
 
+    $timeout = $null
     $download_complete = $false
     while ($download_complete -eq $false) {
         $download_complete = get_payload
+        $timeout += 1
+        if ($timeout -ge 120) {
+            throw('The wait for payload driver software timed out and failed (10 minutes)')
+            error_handler($error)
+            exit
+        }
     }
 
     $install_path = get_install_path
     $atk_install_success = install_atk
     $smartgesture_success = install_smartgesture($install_path)
     if ($atk_install_success -or $smartgesture_success -eq 'error') {
-        $err_count = $error.count
-        $err_string = "$err_count errors occured. See verbose log below:"
-        $err_string,"`n",$error | out-file -filepath 'C:\temp\awscript\TpadDriver_install_error.log'
+        error_handler($error)
     }
 }
 
@@ -28,12 +33,12 @@ function get_payload {
 <# checks to see if all dependency files are fnished downloading 
     from MDM in current directory #>
     start-sleep(5)
-    $evaluate = test-path "C:\temp\awscript\SETUP.CAB", 
-            "C:\temp\awscript\setup.exe", 
-            "C:\temp\awscript\SetupTPDriver.msi", 
-            "C:\temp\awscript\D3F.exe",
-            "C:\temp\awscript\409.msi",
-            "C:\temp\awscript\update"
+    $evaluate = test-path "C:\temp\awscript\gs\SETUP.CAB", 
+            "C:\temp\awscript\gs\setup.exe", 
+            "C:\temp\awscript\gs\SetupTPDriver.msi", 
+            "C:\temp\awscript\gs\D3F.exe",
+            "C:\temp\awscript\gs\409.msi",
+            "C:\temp\awscript\gs\update"
     
     if ($false -in $evaluate) {
         return $false
@@ -59,7 +64,7 @@ function get_install_path {
 
 function install_atk {
     
-    start-process "msiexec.exe" -argumentlist "/i C:\temp\awscript\409.msi /qn /norestart" -Wait
+    start-process "msiexec.exe" -argumentlist "/i C:\temp\awscript\gs\409.msi /qn /norestart" -Wait
     if ($error) {
         return 'error'
     }
@@ -68,12 +73,19 @@ function install_atk {
 function install_smartgesture($install_path) {
 
     start-process "msiexec.exe" -argumentlist "/x {938CFBD4-0652-49E5-BB8B-153948865941} /qn /norestart"
-    start-process "msiexec.exe" -argumentlist "/i C:\temp\awscript\SetupTPDriver.msi /qn /norestart"
-    start-process "C:\temp\awscript\D3F.exe"
+    start-process "msiexec.exe" -argumentlist "/i C:\temp\awscript\gs\SetupTPDriver.msi /qn /norestart"
+    start-process "C:\temp\awscript\gs\D3F.exe"
     md $install_path -Force
     copy-item '.\update' -Force -destination "$install_path"
     if ($error) {
         return 'error'
     }
+}
+
+function error_handler($error) {
+
+    $err_count = $error.count
+    $err_string = "$err_count errors occured. See verbose log below:"
+    $err_string,"`n",$error | out-file -filepath 'C:\temp\awscript\gs\TpadDriver_install_error.log'
 }
 main
