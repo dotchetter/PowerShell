0a747eb8087e4c5e6c58a7692f5615467844aba7
dotchetter <dotchetter@protonmail.ch>
2018-12-30 04:02:46 +0100

    Function calls implemented to clean up code, bug fixes

--- numbR/main.ps1
+++ numbR/main.ps1
@@ -1,52 +1,71 @@
 # Developed for Advania Sverige AB by Simon Olofsson
-# Module file for application "NumbR"
+# Main module file for application "NumbR"
+# module purpose: main
 
 
-# Initialize variable
-$global:all_cost_sum = 0
+# Load framework and GUI dependencies
+[void][system.reflection.assembly]::loadwithpartialname("system.windows.forms") 
+add-type -assemblyname presentationcore,presentationframework
+add-type -name window -namespace console -memberdefinition '
+[DllImport("Kernel32.dll")]
+public static extern IntPtr GetConsoleWindow();
+[DllImport("user32.dll")]
+public static extern bool ShowWindow(IntPtr hWnd, Int32 nCmdShow);'
 $install_path = "$env:userprofile\git\powershell\NumbR"
+$bg_img = [system.drawing.image]::fromfile("$install_path\meta\bg_2.png")
+$icon = "$install_path\meta\numbr.ico"
 
 
 # Import modules
-. ($psscriptroot + "\gui.ps1")
+. ($psscriptroot + "\lp_gui.ps1")
+. ($psscriptroot + "\rp_gui.ps1")
 . ($psscriptroot + "\logic.ps1")
-. ($psscriptroot + "\_event_listnrs.ps1")
+. ($psscriptroot + "\event_listnrs.ps1")
 
 
 
-# Read json file and create instance object
+# --- Initialize variables
 $json = cat "$env:userprofile\git\powershell\NumbR\data.json" | convertfrom-json
+$global:labour = 0
+$global:shipping = 0
+$global:upper_limit = 0
+$global:lower_limit = 0
+$global:upper_multiplicand = 0
+$global:lower_multiplicand = 0
+
+# Function calls
+hide_console
+set_global_values
+set_rpane_values
 
 
+# Convert values from string to Int32
+$global:labour = (0 + $global:labour)
+$global:shipping = (0 + $global:shipping)
+$global:upper_limit = (0 + $global:upper_limit)
+$global:lower_limit = (0 + $global:lower_limit)
+$global:upper_multiplicand = (0 + $global:upper_multiplicand)
+$global:lower_multiplicand = (0 + $global:lower_multiplicand)
 
-<# Load values for mathematic calculations.
-Multiplicands, shipping and labour costs are loaded from json.
 
-$multi_x = operand is <= 499, $multi_y = operand is >= 500 
-mac_increment refers to the increased labour cost for mac repair. #>
-
-# Set state depending on radio buttons
-if ($state_checkbox_mac.checked) {
-    $labour_cost = 'maclabour'
-} else {
-    $labour_cost = 'pclabour'
+<# Verify that lower limit is not greater than 
+upper limit operand and vice versa #>
+if ($lower_limit -ge $upper_limit) {
+    $lower_limit = ($upper_limit - 1)
+} elseif ($upper_limit -le $lower_limit) {
+    $upper_limit = $lower_limit + 1
 }
 
 
-$multi_x = load_data 'x' $json
-$multi_y = load_data 'y' $json
-$ship_cost = load_data 'ship' $json
-$labour_cost = load_data $labour_cost $json
+# Fill boxes in right GUI pane with values
 
 
-
-# Click functionality for objects
+# Initialize clicking functionality for objects on gui
 $add_cost.add_click($add_cost_btn_click)
 $reset.add_click($reset_btn_click)
 $sum_button.add_click($sum_btn_click)
 
 
 # Form loop
-hide_console
 [system.windows.forms.application]::enablevisualstyles();
 $form.showdialog()
