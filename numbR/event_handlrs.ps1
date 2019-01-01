fb69a42cc584a9e8ff0cf071078520fc4bba7271
dotchetter <dotchetter@protonmail.ch>
2019-01-02 00:34:20 +0100

    added messsage prompt when saving. Will move to separate function for general use.

--- numbR/event_handlrs.ps1
+++ numbR/event_handlrs.ps1
@@ -30,7 +30,6 @@ function add_cost() {
 
 
 
-
 function sum_btn() {
 
     $gross_sum = 
@@ -47,5 +46,59 @@ function sum_btn() {
     $rpane_list.items.add("----------------------`n")
     $rpane_list.items.add("Frakt: $shipping Kr")
     $rpane_list.items.add("Arbete: $labour Kr")
+
+}
+
+
+
+function reset_app() {
+    # Reset application for accepting new calculation.
+
+    set_global_values
+    set_rpane_values
+    $rpane_list.items.clear()
+    $lpane_list.items.clear()
+    $net_sum_box.text = '0,00 Kr'
+    $gross_sum_box.text = '0,00 Kr'
+    $lpane_list.text = $null
+    $sum_button.enabled = $true
+    $add_cost.enabled = $true
+}
+
+
+
+function save_data($json) {
+    <# Read data from input boxes in right gui pane, renderd on chalkboard
+    and save values to JSON file. #>
+
+    $state = get_state
+    $color_mode = get_color_mode
+
+    $json.laststate = $state
+    $json.$state.labour = $labour_box.text
+    $json.$state.shipping = $ship_cost_box.text
+    $json.$state.upper_limit = $upper_limit_box.text
+    $json.$state.lower_limit = $lower_limit_box.text
+    $json.$state.upper_multiplicand = $upper_multiplicand_box.text
+    $json.$state.lower_multiplicand = $lower_multiplicand_box.text
+    
+    if ($color_mode -eq 'bright') {
+        $json.darkmode = 0
+    } else {
+        $json.darkmode = 1
+    }
+
+    if ($rounding_on_checkbox.checked -eq $true) {
+        $json.rounding = 1
+    } else {
+        $json.rounding = 0
+    }
+
+    $json | convertto-json | out-file "$env:userprofile\git\powershell\NumbR\data.json"
+    
+    [windows.forms.messagebox]::show("Inställningar för $state har sparats.", "", 
+    [windows.forms.messageboxbuttons]::Ok, [windows.forms.messageboxicon]::information)
+    
+    reset_app
 
 }
--- numbR/logic.ps1
+++ numbR/logic.ps1
@@ -89,39 +89,6 @@ function load_data($state, $value, $json) {
 }
 
 
-function save_data($json) {
-    <# Read data from input boxes in right gui pane, renderd on chalkboard
-    and save values to JSON file. #>
-
-    $state = get_state
-    $color_mode = get_color_mode
-
-    $json.laststate = $state
-    $json.$state.labour = $labour_box.text
-    $json.$state.shipping = $ship_cost_box.text
-    $json.$state.upper_limit = $upper_limit_box.text
-    $json.$state.lower_limit = $lower_limit_box.text
-    $json.$state.upper_multiplicand = $upper_multiplicand_box.text
-    $json.$state.lower_multiplicand = $lower_multiplicand_box.text
-    
-    if ($color_mode -eq 'bright') {
-        $json.darkmode = 0
-    } else {
-        $json.darkmode = 1
-    }
-
-    if ($rounding_on_checkbox.checked -eq $true) {
-        $json.rounding = 1
-    } else {
-        $json.rounding = 0
-    }
-
-    $json | convertto-json | out-file "$env:userprofile\git\powershell\NumbR\data.json"
-    reset_app
-
-}
-
-
 function hide_console() {
     # Hide PS console window during runtime
 
@@ -221,18 +188,3 @@ function set_clipboard() {
     }
 }
 
-
-
-function reset_app() {
-    # Reset application for accepting new calculation.
-
-    set_global_values
-    set_rpane_values
-    $rpane_list.items.clear()
-    $lpane_list.items.clear()
-    $net_sum_box.text = '0,00 Kr'
-    $gross_sum_box.text = '0,00 Kr'
-    $lpane_list.text = $null
-    $sum_button.enabled = $true
-    $add_cost.enabled = $true
-}
