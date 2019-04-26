e6de171c7dbae85665899e974058d47495abbcc5
dotchetter <dotchetter@protonmail.ch>
2019-04-26 11:34:53 +0200

    Initial commit

--- Get-CrosLastUser/Get-CrosLastUserUnmanaged.ps1
+++ Get-CrosLastUser/Get-CrosLastUserUnmanaged.ps1
@@ -1,35 +1,13 @@
-<# 
-$author = Simon Olofsson
-$date = 2019-04-03
-
-This script will:
-
-1. Query GAM to fetch all devices in the G Suite domain
-2. Ask GAM to fetch device info for each device fetched
-3. Parse the list of info, and only keep data of interest
-4. Output a JSON file with the device serialnumber and last user
-
-This script has the following prerequisites:
-
-1. GAM is installed and authenticated on the local system
-2. Write access to $home directory 
-#>
-
 # Current date in string format
 $date = Get-Date -Format 'yyyy-MM-dd'
-
 # Counter variable for progressbar
 $count = 0
-
 # Parse file containing device ID's
-$devices = get-content ".\devices_to_query.dat"
-
+$devices = get-content ".\UnmanagedDevices_to_query.dat"
 # Length variable for progressbar
 $len = $devices.count
-
 # Hashtable to append to
 $output = @{}
-
 # Iterate through the list of device ID's and get data for every device
 foreach ($i in $devices) {
     $q = gam info cros $i recentusers serialnumber
@@ -41,24 +19,23 @@ foreach ($i in $devices) {
     foreach ($u in $q) {
         if ($u -match "email") {
             $user = $u[13..$u.Length] -join ""
-            break
+            if ($user -ne 'UnmanagedUser') {
+                break
+            }
         }
     }
     if($sn -notin $output.keys){
         $output.add($sn, $user)
     }
-    Write-Progress -activity "Working" -status “$count / $len” -PercentComplete (($count / $devices.count)*100)
+    Write-Progress -activity "Working" -status "$count / $len" -PercentComplete (($count / $devices.count)*100)
     $count++
 }
-
 # Convert the hastable to JSON object
 $output = $output | ConvertTo-JSON
-
 # Assert output directory presence
 $exists = Test-Path $home\desktop\Get-CrosLastUser
 if ($exists -ne $true) {
     mkdir $home\desktop\Get-CrosLastUser
 }
-
 # Output a JSON file
-$output | Out-File "$home\desktop\Get-CrosLastUser\$date.json"
+$output | Out-File "$home\desktop\Get-CrosLastUser\UnmanagedUserCorrected.json"
