7a02e1eeada3581e9c36c7197c735f01f0143a20
Simon Olofsson <36161882+dotchetter@users.noreply.github.com>
2019-05-30 10:48:26 +0200

    Removed disclaimer document section

--- AutomatedDISM/diskpart_init.ps1
+++ AutomatedDISM/diskpart_init.ps1
@@ -1,11 +1,2 @@
-# Softare purpose: Bootable image deployment environment for Jönköping Kommun
-
-<# 
-$author = 'Simon Olofsson'
-2019-02-25 Advania Sverige AB
-This source code is not for public use. Any attempts to modify,
-reverse-engineer or otherwise tamper with this software is strictly
-prohibited. 
-#>
 
 start-process "diskpart" -windowstyle hidden -argumentlist "/s X:\Windows\System32\scripts\Windows10.txt" 
